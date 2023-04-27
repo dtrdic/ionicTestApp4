46864fc4d6f063a5c1f15bfe5ae3cc0c2dae8a24
dtrdic <david@nevercode.io>
2023-04-27 10:33:05 +0200

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.ionictest4',
+  appName: 'ionictest4',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
